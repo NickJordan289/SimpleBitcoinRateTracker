bfc95a5d82b884c7385f4cb6c52fe75b04af01a0
Nicholas Jordan <nickjordan289@gmail.com>
2019-07-31 17:19:24 +0800

    Added exception management to keep program running

--- BitcoinRateTracker.py
+++ BitcoinRateTracker.py
@@ -43,34 +43,38 @@ if __name__ == '__main__':
 	os.system('color')
 	old_price = None
 	while True:
-		q = requests.get('https://api.coindesk.com/v1/bpi/currentprice/aud.json').json()
-		new_price = q['bpi']['AUD']['rate_float']
-		
-		if old_price is not None:
-			diff = new_price - old_price
-			if diff > 0:
-				diff = f'+{diff}'
-				print(f'({Fore.GREEN}{diff}{Style.RESET_ALL}) {new_price} AUD - ${btc_bal*new_price}')
-			else:	
-				print(f'({Fore.RED}{diff}{Style.RESET_ALL}) {new_price} AUD - ${btc_bal*new_price}')
-		else:
-			print(f'${new_price} AUD - ${btc_bal*new_price}')
+		try: # keeps program running in the case of an exception
+			q = requests.get('https://api.coindesk.com/v1/bpi/currentprice/aud.json').json()
+			new_price = q['bpi']['AUD']['rate_float']
 			
-		if new_price <= threshold[0]:
-			toaster.show_toast(f'{APP_TITLE} - Threshold Alert', f"Bitcoin is now worth less than ${threshold[0]} AUD", threaded=True, icon_path=None, duration=3)
-		elif new_price >= threshold[1]:
-			toaster.show_toast(f'{APP_TITLE} - Threshold Alert', f"Bitcoin is now worth more than ${threshold[1]} AUD", threaded=True, icon_path=None, duration=3)
-		
-		
-		old_price = new_price
-		
-		date_str = q['time']['updated']
-		date_obj = datetime.strptime(date_str, '%b %d, %Y %H:%M:%S UTC')
-		date_unix = calendar.timegm(date_obj.timetuple()) + 90
-		cur_date_unix = calendar.timegm(time.gmtime())
-		
-		sleep_time = date_unix-cur_date_unix
-		#print(f'Sleeping for {sleep_time}s')
-		if(sleep_time <= 0): # cant have negative values
-			sleep_time = 0.1 # play it safe with small delay
+			if old_price is not None:
+				diff = new_price - old_price
+				if diff > 0:
+					diff = f'+{diff}'
+					print(f'({Fore.GREEN}{diff}{Style.RESET_ALL}) {new_price} AUD - ${btc_bal*new_price}')
+				else:	
+					print(f'({Fore.RED}{diff}{Style.RESET_ALL}) {new_price} AUD - ${btc_bal*new_price}')
+			else:
+				print(f'${new_price} AUD - ${btc_bal*new_price}')
+				
+			if new_price <= threshold[0]:
+				toaster.show_toast(f'{APP_TITLE} - Threshold Alert', f"Bitcoin is now worth less than ${threshold[0]} AUD", threaded=True, icon_path=None, duration=3)
+			elif new_price >= threshold[1]:
+				toaster.show_toast(f'{APP_TITLE} - Threshold Alert', f"Bitcoin is now worth more than ${threshold[1]} AUD", threaded=True, icon_path=None, duration=3)
+			
+			
+			old_price = new_price
+			
+			date_str = q['time']['updated']
+			date_obj = datetime.strptime(date_str, '%b %d, %Y %H:%M:%S UTC')
+			date_unix = calendar.timegm(date_obj.timetuple()) + 90
+			cur_date_unix = calendar.timegm(time.gmtime())
+			
+			sleep_time = date_unix-cur_date_unix
+			#print(f'Sleeping for {sleep_time}s')
+			if(sleep_time <= 0): # cant have negative values
+				sleep_time = 0.1 # play it safe with small delay
+		except Exception as e:
+			print("error retrieving data... Sleeping for 60 seconds")
+			sleep_time=60
 		countdown(sleep_time)
--- EthereumRateTracker.py
+++ EthereumRateTracker.py
@@ -44,35 +44,39 @@ if __name__ == '__main__':
 	os.system('color')
 	old_price = None
 	while True:
-		q = requests.get('https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=AUD&api_key={api_key}').json()
-		new_price = q['AUD']
-		
-		if old_price is not None:
-			diff = new_price - old_price
-			if diff > 0:
-				diff = f'+{diff}'
-				print(f'({Fore.GREEN}{diff}{Style.RESET_ALL}) {new_price} AUD - ${eth_bal*new_price}')
-			else:	
-				print(f'({Fore.RED}{diff}{Style.RESET_ALL}) {new_price} AUD - ${eth_bal*new_price}')
-		else:
-			print(f'${new_price} AUD - ${eth_bal*new_price}')
+		try: # keeps program running in the case of an exception
+			q = requests.get('https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=AUD&api_key={api_key}').json()
+			new_price = q['AUD']
 			
-		if new_price <= threshold[0]:
-			toaster.show_toast(f'{APP_TITLE} - Threshold Alert', f"Ethereum is now worth less than ${threshold[0]} AUD", threaded=True, icon_path=None, duration=3)
-		elif new_price >= threshold[1]:
-			toaster.show_toast(f'{APP_TITLE} - Threshold Alert', f"Ethereum is now worth more than ${threshold[1]} AUD", threaded=True, icon_path=None, duration=3)
-		
-		
-		old_price = new_price
-		
-		#date_str = q['time']['updated']
-		#date_obj = datetime.strptime(date_str, '%b %d, %Y %H:%M:%S UTC')
-		#date_unix = calendar.timegm(date_obj.timetuple()) + 90
-		#cur_date_unix = calendar.timegm(time.gmtime())
-		
-		#sleep_time = date_unix-cur_date_unix
-		#print(f'Sleeping for {sleep_time}s')
-		#if(sleep_time <= 0): # cant have negative values
-		#	sleep_time = 0.1 # play it safe with small delay
-		sleep_time = 61
+			if old_price is not None:
+				diff = new_price - old_price
+				if diff > 0:
+					diff = f'+{diff}'
+					print(f'({Fore.GREEN}{diff}{Style.RESET_ALL}) {new_price} AUD - ${eth_bal*new_price}')
+				else:	
+					print(f'({Fore.RED}{diff}{Style.RESET_ALL}) {new_price} AUD - ${eth_bal*new_price}')
+			else:
+				print(f'${new_price} AUD - ${eth_bal*new_price}')
+				
+			if new_price <= threshold[0]:
+				toaster.show_toast(f'{APP_TITLE} - Threshold Alert', f"Ethereum is now worth less than ${threshold[0]} AUD", threaded=True, icon_path=None, duration=3)
+			elif new_price >= threshold[1]:
+				toaster.show_toast(f'{APP_TITLE} - Threshold Alert', f"Ethereum is now worth more than ${threshold[1]} AUD", threaded=True, icon_path=None, duration=3)
+			
+			
+			old_price = new_price
+			
+			#date_str = q['time']['updated']
+			#date_obj = datetime.strptime(date_str, '%b %d, %Y %H:%M:%S UTC')
+			#date_unix = calendar.timegm(date_obj.timetuple()) + 90
+			#cur_date_unix = calendar.timegm(time.gmtime())
+			
+			#sleep_time = date_unix-cur_date_unix
+			#print(f'Sleeping for {sleep_time}s')
+			#if(sleep_time <= 0): # cant have negative values
+			#	sleep_time = 0.1 # play it safe with small delay
+			sleep_time = 240
+		except Exception as e:
+			print("error retrieving data... Sleeping for 60 seconds")
+			sleep_time=60
 		countdown(sleep_time)
